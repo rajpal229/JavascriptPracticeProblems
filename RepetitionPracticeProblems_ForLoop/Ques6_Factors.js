196b499ab71b6ca6c9208373eb6fc202a4a30943
rajpal229 <rajpalrawat229@gmail.com>
2023-01-21 08:12:18 +0530

    Refactor: Ques6

--- RepetitionPracticeProblems_ForLoop/Ques6_Factors.js
+++ RepetitionPracticeProblems_ForLoop/Ques6_Factors.js
@@ -4,8 +4,20 @@ let number = readlineSync.questionInt("Enter Number: ");
 
 for (var i =2; i < number; i++)
     {
+        let isPrime = true;
         if (number % i == 0)
         {
-            console.log(i + " is a factor of " + number)
+            for (var j =2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    console.log(i + " is Prime Factor")
+                }
         }
     }
